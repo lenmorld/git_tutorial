9811002392528fcbfe23e8f863f75179b9ffd410
lenmor <lenmorld@live.com>
2016-01-09 13:49:57 -0500

    lenny edited script

--- tutorial-shell/tutorial-shell/count.sh
+++ tutorial-shell/tutorial-shell/count.sh
@@ -1,9 +1,8 @@
 # Display the name of each data file and count
 # the number of distinct occurrences of each species
 # in that file
-# lenny edited something
 for filename in data/*.txt
 do
     echo $filename
-    grep -v Species $filename | cut -d, -f2 | sort -n -r | uniq -c
+    grep -v Species $filename | cut -d, -f2 | sort | uniq -c
 done
